1fa1dc8698207dafa4843f83a143eb680850671d
Sikander Khan <julekha1347@gmail.com>
2026-06-12 22:13:14 +0530

    Add files via upload

---
+++ asignment1.py
@@ -0,0 +1,38 @@
+# 2 
+name = "Gargi"
+age = 20
+city = "Jaipur"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+# 3  
+name = input("Enter your name: ")
+
+print("Name in uppercase:", name.upper())
+print("Total characters:", len(name))
+
+
+# 5
+
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+print("Complete List:", fruits)
+print("First Element:", fruits[0])
+print("Last Element:", fruits[-1])
+print("Total Items:", len(fruits))
+
+
+#  6
+
+numbers = [10, 20, 30, 40, 50]
+
+numbers.append(60)   # Add 60
+numbers.remove(20)   # Remove 20
+
+print("Updated List:", numbers)
+
+
+ 
